2bf75a6818e6ab00d843ad4c513676a3bc3f0038
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2024-04-16 10:32:00 -0300

    feat: main executa teste de registerbank

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include "systemc.h"
 //#include "tests/ula_testbench.h"
 //#include "tests/test1_tests.h"
-#include "tests/test2_tests.h"
+// #include "tests/test2_tests.h"
+#include "tests/registerbank_test.h"
 
 int sc_main(int argc, char* argv[]) {
 
-	test_2();
+	registerbank_test();
 
 	return 0;
 }
