fedf6bf180ef0ef82a2ac8bb7798f2daef4d966e
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2024-05-07 11:39:28 -0300

    fix: remove cout mux

--- components/mux.h
+++ components/mux.h
@@ -12,8 +12,6 @@ SC_MODULE(mymux) {
 
     void m() {
         S.write(sel.read() ? in2 : in1);
-
-        std::cout << "MUX: " << sel.read() << " " << in2 << " " << in1 << " out: "  << S.read() << '\n';
     }
 
     SC_CTOR(mymux) {
